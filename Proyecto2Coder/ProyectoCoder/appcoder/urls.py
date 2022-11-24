be8839107943b90030a60aa85b3968c9109502d0
Bruno <brunobini96@gmail.com>
2022-11-24 09:12:32 -0300

    Terminada primer playground

--- Proyecto2Coder/ProyectoCoder/appcoder/urls.py
+++ Proyecto2Coder/ProyectoCoder/appcoder/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from appcoder.views import curso1,inicio, listaProfesores,crea_profesor,eliminarProfesor,editar_profesor
+from appcoder.views import curso1,inicio, listaProfesores,crea_profesor,eliminarProfesor,editar_profesor,CursoLista,CursoDetail,CursoCreate,CursoDelete,CursoUpdate
 from appcoder.views import lista_curso,Cursos2,Profesores2,Entregables2,Estudiantes2,cursoFormulario,busqueda_camada,buscar,listaCursos
 
 
 urlpatterns = [
-    path("", inicio),
+    path("", inicio , name = 'Inicio'),
     path("agregar-curso/<nombre1>/<camada1>", curso1),
     path("lista-cursos", lista_curso),
     path("Cursos", Cursos2, name="Cursos"),
@@ -20,4 +20,10 @@ urlpatterns = [
     path('crea-profesor/', crea_profesor, name = "CreaProfesor"),
     path('elimina-profesor/<int:id>', eliminarProfesor, name = "EliminaProfesor"),
     path('edita-profesor/<int:id>', editar_profesor, name = "EditarProfesor"),
+    path('listaCursos/', CursoLista.as_view(), name = "ListaCursos"),
+    path('detalleCurso/<pk>', CursoDetail.as_view(), name = "DetalleCurso"),
+    path('creaCurso/', CursoCreate.as_view(), name = "CreaCurso"),
+    path('actualizarCurso/<pk>', CursoUpdate.as_view(), name = "ActualizaCursos"),
+     path('eliminarCurso/<pk>', CursoDelete.as_view(), name = "EliminaCursos"),
 ]
+
--- Proyecto2Coder/ProyectoCoder/appcoder/views.py
+++ Proyecto2Coder/ProyectoCoder/appcoder/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from .models import Curso, Profesor
 from  django.http import HttpResponse
+from django.views.generic import ListView,DeleteView,CreateView,UpdateView,DetailView
 # Create your views here.
 
 def curso1(request,nombre1,camada1):
@@ -111,12 +112,54 @@ def editar_profesor(request,id):
             return redirect('ListaProfesores')
     else:
         mi_formulario = ProfesorFormulario(initial={"nombre": profesor.nombre,"apellido": profesor.apellido,"email":profesor.email,"profesion":profesor.profesion})
-    return render(request,"profesorFormulario.html", {'mi_formulario': mi_formulario})
+    return render(request,"editarProfesor.html", {'mi_formulario': mi_formulario, 'id':profesor.id})
 
     
-   
-        
+class CursoLista(ListView):
     
+    model = Curso
+    
+    template_name = 'curso_list.html' 
+    
+    context_object_name = "cursos"
+        
+class CursoDetail(DetailView):
+    
+    model = Curso
+    
+    template_name = 'curso_detail.html' 
+    
+    context_object_name = "curso"
+
+class CursoCreate(CreateView):
+    
+    model = Curso
+    
+    template_name = 'curso_create.html'
+    
+    fields = ('__all__')
+    success_url = '/app-coder/'
+    
+
+class CursoUpdate(UpdateView):
+    
+    model = Curso
+    
+    template_name = 'curso_update.html'
+    
+    fields = ('__all__')
+    success_url = '/app-coder/'
+    
+
+class CursoDelete(DeleteView):
+    
+    model = Curso
+    
+    template_name = 'curso_delete.html' 
+    
+    success_url = '/app-coder/'
+
+
         
     
             
